3a7a9b15ee61c0401214675178182f4e1b62ba5f
IsaaczZj <joseisaac.isaac123@gmail.com>
2025-05-02 18:48:03 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import Form from "./components/Form";
+
+function App() {
+  return (
+    <>
+      <h1>Formulario</h1>
+      <Form />
+    </>
+  );
+}
+
+export default App;
