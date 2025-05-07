a57f2817f55b16d9e30bfcd115307b9528803222
IsaaczZj <joseisaac.isaac123@gmail.com>
2025-05-07 14:40:17 -0300

    criando schema de validacao

---
+++ src/schemas/userFormSchema.ts
@@ -0,0 +1,27 @@
+import { z } from "zod";
+
+export const userFormSchema = z
+  .object({
+    name: z.string().min(3, "O nome precisa ter mais de 3 caracteres"),
+    lastname: z.string().min(3, "O sobrenome precisa ter mais de 3 caracteres"),
+    gender: z.string().refine((field) => field !== "select", {
+      message: "Você precisa escolher um genero",
+    }),
+    email: z
+      .string()
+      .min(1, "O campo nao é obrigatorio")
+      .email("Insira um email válido"),
+    password: z.string().min(6, "A senha precisa ter no minimo 6 caracteres"),
+    confirmpassword: z
+      .string()
+      .min(6, "A confirmação precisa ter no mínimo 6 caracteres"),
+    agree: z.boolean().refine((field) => field === true, {
+      message: "O campo precisa concordar com os termos",
+    }),
+  })
+  .refine((field) => field.password === field.confirmpassword, {
+    message: "As senhas precisam ser iguais",
+    path: ["confirmpassword"],
+  });
+
+export type UserFormSchema = z.infer<typeof userFormSchema>
