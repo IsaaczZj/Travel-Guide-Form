712dac76ec5c4d09b1d2e82d16a7465424971601
IsaaczZj <joseisaac.isaac123@gmail.com>
2025-05-08 15:49:04 -0300

    ajustando imagem

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="bg-zinc-300 min-h-screen w-full flex flex-col items-center justify-center gap-6 pt-8 ">
       <h1 className="text-4xl text-emerald-900 font-bold">Travel Guide</h1>
-      <div className="flex gap-5 items-center bg-white p-7 md:w-3xl md:p-0 md:h-[750px] h-[650] w-[450px] px-4">
+      <div className="flex gap-5 items-center bg-white p-7 md:w-3xl md:p-0 md:h-[750px] h-[650] w-auto px-4">
         <img
           className="hidden md:block h-full w-[400px] object-cover "
           src={travelMen}
