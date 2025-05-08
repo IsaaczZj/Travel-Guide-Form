3fe55b786fff0c220fa1f1022e564477cfc209d8
IsaaczZj <joseisaac.isaac123@gmail.com>
2025-05-08 15:47:37 -0300

    ajustando largura dos elementos

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import Form from "./components/Form";
 import travelMen from "./assets/men_walking.jpeg";
 function App() {
   return (
-    <div className="bg-zinc-300 min-h-screen w-full flex flex-col items-center justify-center gap-6 pt-8">
+    <div className="bg-zinc-300 min-h-screen w-full flex flex-col items-center justify-center gap-6 pt-8 ">
       <h1 className="text-4xl text-emerald-900 font-bold">Travel Guide</h1>
-      <div className="flex gap-5 items-center bg-white p-5 sm:w-3xl sm:p-0 h-[750px] w-[450px]">
+      <div className="flex gap-5 items-center bg-white p-7 md:w-3xl md:p-0 md:h-[750px] h-[650] w-[450px] px-4">
         <img
-          className="hidden sm:block h-full w-[400px] object-cover "
+          className="hidden md:block h-full w-[400px] object-cover "
           src={travelMen}
           alt=""
         />
